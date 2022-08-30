4ef79995a525f16acd8c1f1f2adf4317eb8d83d9
Ramon Trepat <r.trepis@gmail.com>
2022-08-30 17:41:53 +0200

    Borrat  react setup y afegit component registerForm

---
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,32 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import RegisterFormStyled from "./RegisterFormStyled";
+
+const RegisterForm = () => {
+  return (
+    <RegisterFormStyled>
+      <Form.Group className="mb-3" controlId="userName">
+        <Form.Label>Nom usari</Form.Label>
+        <Form.Control type="text" placeholder="Introduïu el vostre nom" />
+        <Form.Text className="text-muted">
+          Introduïu un nom alfanumèric més llarg de 3 digits i menys de 30.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Label>Contrasenya</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Introduïu la vostra contrasenyes"
+        />
+        <Form.Text className="text-muted">
+          Introduïu un nom alfanumèric més llarg de 3 digits i menys de 30.
+        </Form.Text>
+      </Form.Group>
+      <Button variant="success" type="submit">
+        Regitra't
+      </Button>
+    </RegisterFormStyled>
+  );
+};
+
+export default RegisterForm;
