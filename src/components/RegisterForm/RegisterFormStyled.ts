c5e6d232ebe50e41ec130f606b8e15d48078f183
Ramon Trepat <r.trepis@gmail.com>
2022-08-30 22:40:47 +0200

    Added register form css

--- src/components/RegisterForm/RegisterFormStyled.ts
+++ src/components/RegisterForm/RegisterFormStyled.ts
@@ -1,11 +1,36 @@
 import styled from "styled-components";
 
-const RegisterFormStyled = styled.form`
+const RegisterFormStyled = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  .register-form  {
+    max-width: 335px;
+    padding: 15px;
+    border: 2px green solid;
+    border-radius: 15px;
+
+    &__input {
+      background-color: #e9e9e9;
+    }
+
+    &__button {
+      font-family: inherit;
+      font-size: inherit;
+      font-weight: inherit;
+      background-color: green;
+      width: 160px;
+      height: 55px;
+    }
+
+    &__footer {
+      margin-top: 50px;
+      text-align: center;
+    }
+  }
 `;
 
 export default RegisterFormStyled;
