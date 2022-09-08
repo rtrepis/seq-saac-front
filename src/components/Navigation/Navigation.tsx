a740cb7d999655bb464f45fb2e0f538d9e2ba4fe
Ramon Trepat <r.trepis@gmail.com>
2022-09-09 00:36:40 +0200

    Deleted unused import

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,4 @@
 import { NavDropdown } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useSelector } from "react-redux";
