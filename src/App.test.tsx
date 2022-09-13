c3499de36da0e57ea6e0c5fcd2b3fb64395988a8
Ramon Trepat <r.trepis@gmail.com>
2022-09-13 17:11:15 +0200

    Tests App component

---
+++ src/App.test.tsx
@@ -0,0 +1,36 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+import { renderUser } from "./utils/test/test-utils-Loggin";
+
+const mockDispatch = jest.fn();
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+beforeEach(() => jest.clearAllMocks());
+
+describe("Given a App component", () => {
+  describe("When user is not logged", () => {
+    test("Then should not called dispatch", () => {
+      renderUser(<App />);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("When user is logged", () => {
+    test("Then should called dispatch", () => {
+      const mockToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwODYxYzk5MGM3MDlhNmNlYjk0NWQiLCJ1c2VyTmFtZSI6InRlc3RpbmciLCJpYXQiOjE2NjIxMzk1Mzd9.EKxxxoIKOLRRPDR4Uuh-_QmFM8khGF4_-mxbIxjrOpE";
+      window.localStorage.setItem("userToken", mockToken);
+
+      renderUser(<App />);
+
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+  });
+});
