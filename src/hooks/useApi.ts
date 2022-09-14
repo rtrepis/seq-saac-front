8e97e553e85b83390bf0248ebd3667c4f9f56396
Ramon Trepat <r.trepis@gmail.com>
2022-09-14 14:00:14 +0200

    Deleted unused import

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { loadSequencesActionCreator } from "../app/slice/sequencesSlice";
 import { loadShowPictogramsActionCreator } from "../app/slice/showPictogramsSlice";
 import { uiModalShowActionCreator } from "../app/slice/uiSlice";
