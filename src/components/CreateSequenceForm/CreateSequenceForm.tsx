9e8c1391145349f4abb217aff4a0d86ffabd0554
Ramon Trepat <r.trepis@gmail.com>
2022-09-14 21:54:19 +0200

    Added const pictogramEmpty and delete key

--- src/components/CreateSequenceForm/CreateSequenceForm.tsx
+++ src/components/CreateSequenceForm/CreateSequenceForm.tsx
@@ -76,11 +76,12 @@ const CreateSequenceForm = () => {
       amount: amountPictograms.amount + operator,
     });
 
+    const pictogramEmpty = 26527;
     operator > 0
       ? dispatch(
           addSelectPictogramActionCreator({
             index: amountPictograms.amount,
-            pictogram: 26527,
+            pictogram: pictogramEmpty,
           })
         )
       : dispatch(deleteSelectPictogramActionCreator());
@@ -152,7 +153,6 @@ const CreateSequenceForm = () => {
             <>
               <Button
                 className="m-2"
-                key={`button${index}`}
                 onClick={() => handleSelectPictogram(index)}
                 id={`button-select-${index}`}
               >
@@ -160,7 +160,6 @@ const CreateSequenceForm = () => {
               </Button>
               <PictogramShow
                 pictogram={selectPictograms[index].pictogram}
-                key={`${selectPictograms[index].index}${selectPictograms[index].pictogram}`}
                 size="small"
               />
             </>
