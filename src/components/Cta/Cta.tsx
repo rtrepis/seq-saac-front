9ce0b037182e261ec36698711d4a04b36d544581
Ramon Trepat <r.trepis@gmail.com>
2023-01-25 11:02:37 +0100

    Added component cta

---
+++ src/components/Cta/Cta.tsx
@@ -0,0 +1,46 @@
+const Cta = (): JSX.Element => {
+  return (
+    <>
+      <div className="bg-primary bg-opacity-25 p-4 d-flex flex-wrap justify-content-around align-items-center border-top border-white border-3 ">
+        <div className="text-center">
+          <img
+            src="/img/cta/crea-200px.jpg"
+            alt="crea"
+            height="200"
+            width="200"
+            className="border-top border-4 border-primary rounded-4"
+          />
+          <h2 className="mt-3 pb-2 fw-bold text-dark border-bottom border-4 border-primary rounded-5">
+            Crea
+          </h2>
+        </div>
+        <div className="text-center">
+          <img
+            src="/img/cta/compartir-200px.jpg"
+            alt="compartir"
+            height="200"
+            width="200"
+            className="border-top border-4 border-primary rounded-4"
+          />
+          <h2 className="mt-3 pb-2 fw-bold text-dark border-bottom border-4 border-primary rounded-5">
+            Comparteix
+          </h2>
+        </div>
+        <div className="text-center">
+          <img
+            src="/img/cta/sequencies-200px.jpg"
+            alt="seqüències"
+            height="200"
+            width="200"
+            className="border-top border-4 border-primary rounded-4"
+          />
+          <h2 className="mt-3 pb-2 fw-bold text-dark border-bottom border-4 border-primary rounded-5">
+            Seqüències
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cta;
